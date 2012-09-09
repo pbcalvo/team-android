5fd67dafc389664581844a2b621efda4ee21bd85
Jorge Chartier <jorgeandreschartier@gmail.com>
2012-09-09 15:40:54 -0300

    diseño de las vistas

--- src/com/familink/AnnouncementsActivity.java
+++ src/com/familink/AnnouncementsActivity.java
@@ -1,114 +1,175 @@
 package com.familink;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
-
 
 public class AnnouncementsActivity extends Activity {
 
 	Button newActivityButton;
 	Button journal;
 	Button message;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_announcements);
-        
-        newActivityButton = (Button) findViewById(R.id.add_announcement_button);
-        newActivityButton.setOnClickListener(new View.OnClickListener() {
-			
+	LinearLayout announcementsLinearLayout;
+	List<RelativeLayout> layout_buttons;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_announcements);
+		
+		layout_buttons = new ArrayList<RelativeLayout>();
+
+		announcementsLinearLayout = (LinearLayout) findViewById(R.id.annoucements_list);
+		
+		addAnnouncement("Reuni—n de apoderados", "Este Lunes 10 de septiembre nos juntaremos en la sala 12", Calendar.getInstance());
+		
+
+		newActivityButton = (Button) findViewById(R.id.announcements_add_button);
+		newActivityButton.setOnClickListener(new View.OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
-				View view = getLayoutInflater().inflate( R.layout.new_announcement, null );
-				AlertDialog.Builder builder = new AlertDialog.Builder(AnnouncementsActivity.this);
+				View view = getLayoutInflater().inflate(
+						R.layout.new_announcement, null);
+				AlertDialog.Builder builder = new AlertDialog.Builder(
+						AnnouncementsActivity.this);
 				builder.setTitle(R.string.announcement_new);
-			    builder.setView(view);
-			    
-			    builder.setPositiveButton(R.string.string_send, new DialogInterface.OnClickListener() {
-					
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						dialog.dismiss();
-			            Toast.makeText(AnnouncementsActivity.this, R.string.announcement_sent, Toast.LENGTH_SHORT).show();
-						
-					}
-				});
-			    
-			    
-			    
-			    builder.setNegativeButton(R.string.string_cancel, new DialogInterface.OnClickListener() {
-					
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						dialog.dismiss();
-						
-					}
-				});
-				
-			    builder.create().show();
+				builder.setView(view);
+
+				builder.setPositiveButton(R.string.string_send,
+						new DialogInterface.OnClickListener() {
+
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {
+								dialog.dismiss();
+								Toast.makeText(AnnouncementsActivity.this,
+										R.string.announcement_sent,
+										Toast.LENGTH_SHORT).show();
+
+							}
+						});
+
+				builder.setNegativeButton(R.string.string_cancel,
+						new DialogInterface.OnClickListener() {
+
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {
+								dialog.dismiss();
+
+							}
+						});
+
+				builder.create().show();
 			}
 
-			
 		});
-        
-        /*message = (Button) findViewById(R.id.message_button);
-        message.setOnClickListener(new View.OnClickListener() {
-			
+
+		message = (Button) findViewById(R.id.messages_button);
+		message.setOnClickListener(new View.OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Intent intent = new Intent(getBaseContext(), MessageActivity.class);
-	        	startActivityForResult(intent, 0);	
-	        	finish();
+				Intent intent = new Intent(getBaseContext(),
+						MessageActivity.class);
+				intent.putExtra("GROUP_ID", 1);
+				startActivityForResult(intent, 0);
+				finish();
 			}
 		});
-        journal = (Button) findViewById(R.id.journal_button);
-        journal.setOnClickListener(new View.OnClickListener() {
-			
+
+		journal = (Button) findViewById(R.id.journal_button);
+		journal.setOnClickListener(new View.OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub	
-				Intent intent = new Intent(getBaseContext(), JournalActivity.class);
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(getBaseContext(),
+						JournalActivity.class);
 				intent.putExtra("GROUP_ID", 1);
-	        	startActivityForResult(intent, 0);	
-	        	finish();
+				startActivityForResult(intent, 0);
+				finish();
 			}
-		});*/
-    }
+		});
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_menu_button, menu);
-        return true;
-    }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        switch (item.getItemId()) {
-            case R.id.groups_menu_button:
-                //Volver a la selección de grupos.
-                return true;
-            case R.id.campus_menu_button:
-                //Idem al anterior.
-                return true;
-            case R.id.settings_menu_button:
-            	//Ventana de setting, aún no implementada.
-            	return true; 
-            case R.id.logout_menu_button:
-            	//Logout, tampoco implementado.
-            	return true; 
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    } 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_menu_button, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.groups_menu_button:
+			// Volver a la selección de grupos.
+			return true;
+		case R.id.campus_menu_button:
+			// Idem al anterior.
+			return true;
+		case R.id.settings_menu_button:
+			// Ventana de setting, aún no implementada.
+			return true;
+		case R.id.logout_menu_button:
+			// Logout, tampoco implementado.
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+	
+	public void addAnnouncement(String title, String content, Calendar date) {
+
+		View view = getLayoutInflater().inflate(R.layout.announcement_layout,
+				null);
+
+		TextView content_text = (TextView) view.findViewById(R.id.announcement_content);
+		content_text.setText(content);
+		
+		TextView title_text = (TextView) view.findViewById(R.id.announcement_title);
+		title_text.setText(title);
+
+		TextView date_text = (TextView) view.findViewById(R.id.announcement_date);
+		date_text.setText(date.HOUR + ":" + date.MINUTE + ", today");
+
+		RelativeLayout announcement_layout = (RelativeLayout) view
+				.findViewById(R.id.announcement);
+		announcement_layout.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				/*
+				 * Intent intent = new Intent(getBaseContext(),
+				 * JournalActivity.class); startActivityForResult(intent, 0);
+				 * finish();
+				 */
+			}
+		});
+
+		layout_buttons.add(announcement_layout);
+
+		announcementsLinearLayout.addView(view, 0, new LayoutParams(
+				LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+
+	}
 }
--- src/com/familink/FamilinkAndroidActivity.java
+++ src/com/familink/FamilinkAndroidActivity.java
@@ -15,159 +15,157 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-
 public class FamilinkAndroidActivity extends Activity {
-    /** Called when the activity is first created. */
-	
+	/** Called when the activity is first created. */
+
 	private int group_id = 0;
 	private int kid_id;
-	
-	TextView prueba; 
+
+	TextView prueba;
 	String name_string;
 	TextView title;
 	Button journal;
 	Button message;
 	Button announcement;
 	Button calendar;
-	RelativeLayout kid1;
 	LinearLayout kidsTableLayout;
 	List<String> parents;
 	List<RelativeLayout> layout_buttons;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
-        
-        layout_buttons = new ArrayList<RelativeLayout>();
-        
-        kidsTableLayout = (LinearLayout)findViewById(R.id.kidsList);
-        
-        parents = new ArrayList<String>();
-        parents.add("Papa");
-        parents.add("Mama");
-        
-        addKid("Amanda Solis");
-        addKid("Juan Perez");
-        
-        
-        group_id = this.getIntent().getIntExtra("GROUP_ID", 0);
-        
-        title = (TextView) findViewById(R.id.titulo);
-        title.setText(R.string.journal_title);
-                
-        if(group_id == 0) {
-        	Intent intent = new Intent(getBaseContext(), SelectGroupActivity.class);
-        	startActivityForResult(intent, 0);
-        	finish();
-        }
-        
-        journal = (Button) findViewById(R.id.journal_button);
-        journal.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub		
-			}
-		});
-        
-        message = (Button) findViewById(R.id.message_button);
-        message.setOnClickListener(new View.OnClickListener() {
-			
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home);
+
+		layout_buttons = new ArrayList<RelativeLayout>();
+
+		kidsTableLayout = (LinearLayout) findViewById(R.id.kidsList);
+
+		parents = new ArrayList<String>();
+		parents.add("Papa");
+		parents.add("Mama");
+
+		addKid("Amanda Solis");
+		addKid("Juan Perez");
+
+		group_id = this.getIntent().getIntExtra("GROUP_ID", 0);
+
+		if (group_id == 0) {
+			Intent intent = new Intent(getBaseContext(),
+					SelectGroupActivity.class);
+			startActivityForResult(intent, 0);
+			finish();
+		}
+
+		journal = (Button) findViewById(R.id.journal_button);
+		journal.setOnClickListener(new View.OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Intent intent = new Intent(getBaseContext(), MessageActivity.class);
-	        	startActivityForResult(intent, 0);	
-	        	finish();
 			}
 		});
-        
-        announcement = (Button) findViewById(R.id.announcement_button);
-        announcement.setOnClickListener(new View.OnClickListener() {
-			
+
+		message = (Button) findViewById(R.id.message_button);
+		message.setOnClickListener(new View.OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Intent intent = new Intent(getBaseContext(), AnnouncementsActivity.class);
-	        	startActivityForResult(intent, 0);		
-	        	finish();
-			}
-		});
-        
-        calendar = (Button) findViewById(R.id.calendar_button);
-        calendar.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				//Intent intent = new Intent(getBaseContext(), SelectGroupActivity.class);
-	        	//startActivityForResult(intent, 0);				
-			}
-		});
-        
-    }
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_menu_button, menu);
-        return true;
-    }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        switch (item.getItemId()) {
-            case R.id.groups_menu_button:
-                //Volver a la selección de grupos.
-                return true;
-            case R.id.campus_menu_button:
-                //Idem al anterior.
-                return true;
-            case R.id.settings_menu_button:
-            	//Ventana de setting, aún no implementada.
-            	return true; 
-            case R.id.logout_menu_button:
-            	//Logout, tampoco implementado.
-            	return true; 
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-    
-    public void addKid(String name){
-    	name_string = name;
-    	
-    	View view = getLayoutInflater().inflate(R.layout.kids_layout, null);
-    	
-		TextView name_kid = (TextView) view.findViewById(R.id.name_kid);
-		name_kid.setText(name);
-		
-		TextView parents_kid = (TextView) view.findViewById(R.id.parents_kid);
-		parents_kid.setText(parents.get(0)+" y "+parents.get(1));
-		
-		RelativeLayout layout_kid = (RelativeLayout) view.findViewById(R.id.relative_kid);
-		layout_kid.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent intent = new Intent(getBaseContext(), JournalActivity.class);
-				intent.putExtra("KID_ID", 1);
-				intent.putExtra("KID_NAME", name_string);
-				intent.putExtra("GROUP_ID", group_id);
-				startActivityForResult(intent,0);
+				Intent intent = new Intent(getBaseContext(),
+						MessageActivity.class);
+				startActivityForResult(intent, 0);
 				finish();
 			}
 		});
-		
+
+		announcement = (Button) findViewById(R.id.announcement_button);
+		announcement.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(getBaseContext(),
+						AnnouncementsActivity.class);
+				startActivityForResult(intent, 0);
+				finish();
+			}
+		});
+
+		calendar = (Button) findViewById(R.id.calendar_button);
+		calendar.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				// Intent intent = new Intent(getBaseContext(),
+				// SelectGroupActivity.class);
+				// startActivityForResult(intent, 0);
+			}
+		});
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_menu_button, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.groups_menu_button:
+			// Volver a la selección de grupos.
+			return true;
+		case R.id.campus_menu_button:
+			// Idem al anterior.
+			return true;
+		case R.id.settings_menu_button:
+			// Ventana de setting, aún no implementada.
+			return true;
+		case R.id.logout_menu_button:
+			// Logout, tampoco implementado.
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	public void addKid(String name) {
+		name_string = name;
+
+		View view = getLayoutInflater().inflate(R.layout.kids_layout, null);
+
+		TextView name_kid = (TextView) view.findViewById(R.id.name_kid);
+		name_kid.setText(name);
+
+		TextView parents_kid = (TextView) view.findViewById(R.id.parents_kid);
+		parents_kid.setText(parents.get(0) + " y " + parents.get(1));
+
+		RelativeLayout layout_kid = (RelativeLayout) view
+				.findViewById(R.id.relative_kid);
+		layout_kid.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(getBaseContext(),
+						JournalActivity.class);
+				intent.putExtra("KID_ID", 1);
+				intent.putExtra("KID_NAME", name_string);
+				intent.putExtra("GROUP_ID", group_id);
+				startActivityForResult(intent, 0);
+				finish();
+			}
+		});
+
 		layout_buttons.add(layout_kid);
-		
-		
-		kidsTableLayout.addView(view,0, new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
-    	
-    }
-    
-    
-    
+
+		kidsTableLayout.addView(view, 0, new LayoutParams(
+				LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+
+	}
+
 }
